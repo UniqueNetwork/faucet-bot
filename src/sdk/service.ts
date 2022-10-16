903a6edc22beafaa25c2855c01db26732d69fc0e
ashkuc <ashkucc@gmail.com>
2022-10-16 22:18:38 +0200

    Moved account and sdk to providers.ts

--- src/sdk/service.ts
+++ src/sdk/service.ts
@@ -1,45 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Account } from '@unique-nft/accounts';
-import { KeyringProvider } from '@unique-nft/accounts/keyring';
-import { SignatureType } from '@unique-nft/accounts';
-import { IClient, Sdk } from '@unique-nft/sdk';
-import { KeyringPair } from '@polkadot/keyring/types';
+import { IClient } from '@unique-nft/sdk';
+import { InjectAccount, InjectSdk } from './providers';
 
 @Injectable()
 export class SdkService {
-  private sdk: IClient;
-  private account: Account<KeyringPair>;
-
   private readonly logger = new Logger(SdkService.name);
 
-  constructor(private readonly configService: ConfigService) {}
-
-  private async createSdk() {
-    const keyringProvider = new KeyringProvider({
-      type: SignatureType.Sr25519,
-    });
-
-    await keyringProvider.init();
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    this.account = keyringProvider.addSeed(
-      this.configService.get<string>('seed'),
-    );
-
-    const restUrl = this.configService.get<string>('restUrl');
-
-    this.sdk = new Sdk({
-      signer: this.account,
-      baseUrl: restUrl,
-    });
-  }
+  constructor(
+    @InjectSdk private readonly sdk: IClient,
+    @InjectAccount private readonly account,
+    private readonly configService: ConfigService,
+  ) {}
 
   public async sendTo(destination: string) {
-    if (!this.sdk) {
-      await this.createSdk();
-    }
     const amount = this.configService.get('dropAmount');
     const address = this.account.instance.address;
 
