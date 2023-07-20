d08f3b74dde5059f66d634f0c02c116a23fc72c9
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2023-07-20 17:05:34 +0300

    add get request for get balances

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -6,9 +6,19 @@ export class ApiController {
   private logger = new Logger(ApiController.name);
   constructor(private readonly balancesService: BalancesService) {}
 
+  @Get('/balances')
+  private showBalancesGet() {
+    this.logger.log('Show balances get');
+    try {
+      return this.balancesService.showBalances();
+    } catch (err) {
+      this.logger.log(`Show balances Error ${err.message}`, err);
+    }
+  }
+
   @Post('/balances')
   private showBalances() {
-    this.logger.log('Show balances');
+    this.logger.log('Show balances post');
     try {
       return this.balancesService.showBalances();
     } catch (err) {
